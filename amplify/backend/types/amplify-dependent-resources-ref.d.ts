a33127a50fd7844b3ea9bf0b9df7466c906ee35b
Jonathan T. Cohen <jcohen66g@gmail.com>
2022-10-02 13:09:23 -0400

    unlinked

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,14 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "familyhelper2e5826562e582656": {
-            "HostedUIDomain": "string",
-            "OAuthMetadata": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
