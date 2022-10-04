6f7b960f3cc39f9f7f6c40c2de5da6b01ecdca65
Jonathan T. Cohen <jcohen66g@gmail.com>
2022-10-03 23:11:29 -0400

    Fixed cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,12 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "familyhelper635e2aed635e2aed": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
